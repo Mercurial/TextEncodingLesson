6f0190d70775755f8fa5fe0c19b957c6b6c860b4
Clark Alesna <rawriclark@pop-os.localdomain>
2019-12-30 01:25:46 +0800

    Base Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TextEncodingLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNo = 0;
+            Console.WriteLine("Enter number to convert to 5 bits: ");
+            firstNo = int.Parse(Console.ReadLine());
+            var newFiveBit = new FiveBits(firstNo);
+            Console.WriteLine($"The number you entered was: {newFiveBit.ToInt()}");
+            Console.WriteLine($"The number you entered in char was: {newFiveBit.ToChar()}");
+        }
+    }
+}
